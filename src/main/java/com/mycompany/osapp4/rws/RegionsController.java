519033b79183e4109199e9debf24c1ed7eefe189
nleitefaria <nleitefaria@gmail.com>
2018-10-10 12:16:32 +0100

    Updated RegionsController

--- src/main/java/com/mycompany/osapp4/rws/RegionsController.java
+++ src/main/java/com/mycompany/osapp4/rws/RegionsController.java
@@ -1,5 +1,7 @@
 package com.mycompany.osapp4.rws;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,19 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mycompany.osapp4.entity.Regions;
 import com.mycompany.osapp4.service.impl.RegionsServiceImpl;
 
 @RestController
 public class RegionsController 
 {
 	@Autowired
-	RegionsServiceImpl regionsService;
+	RegionsServiceImpl service;
 
 	@RequestMapping(value = "/regions/count", method = RequestMethod.GET)
 	public ResponseEntity<Long> count()
 	{		
-		return new ResponseEntity<Long>(regionsService.count(), HttpStatus.OK);
+		return new ResponseEntity<Long>(service.count(), HttpStatus.OK);
 	}
-
-
+	
+	@RequestMapping(value = "/regions", method = RequestMethod.GET)
+	public ResponseEntity<List<Regions>> findAll()
+	{		
+		return new ResponseEntity<List<Regions>>(service.findAll(), HttpStatus.OK);
+	}
 }
--- src/main/java/com/mycompany/osapp4/service/impl/RegionsServiceImpl.java
+++ src/main/java/com/mycompany/osapp4/service/impl/RegionsServiceImpl.java
@@ -1,19 +1,30 @@
 package com.mycompany.osapp4.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mycompany.osapp4.entity.Regions;
 import com.mycompany.osapp4.repository.RegionsRepository;
 
 @Service
 public class RegionsServiceImpl {
 	
 	@Autowired
-	private RegionsRepository regionsRepository;
+	private RegionsRepository repository;
 	
+	@Transactional
 	public Long count()
 	{
-		return regionsRepository.count();
+		return repository.count();
+	}
+	
+	@Transactional
+	public List<Regions> findAll()
+	{
+		return repository.findAll();
 	}
 
 
