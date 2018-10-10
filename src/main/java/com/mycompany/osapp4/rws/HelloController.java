c993d7384958519f7d24964d0490bdd4fa0e256a
nleitefaria <nleitefaria@gmail.com>
2018-10-10 10:13:02 +0100

    clean

--- src/main/java/com/mycompany/osapp4/rws/HelloController.java
+++ src/main/java/com/mycompany/osapp4/rws/HelloController.java
@@ -1,14 +1,22 @@
 package com.mycompany.osapp4.rws;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController 
 {
-	@GetMapping(value = "/hello")
-    public String sayHello() {
-        return "hello";
-    }
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public ResponseEntity<String> findAll() {
+		logger.info("Hello");
+		return new ResponseEntity<String>("Hello", HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/mycompany/osapp4/rws/RegionsController.java
+++ src/main/java/com/mycompany/osapp4/rws/RegionsController.java
@@ -3,7 +3,8 @@ package com.mycompany.osapp4.rws;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mycompany.osapp4.service.impl.RegionsServiceImpl;
@@ -14,9 +15,9 @@ public class RegionsController
 	@Autowired
 	RegionsServiceImpl regionsService;
 
-	@GetMapping("/regions/count")
-	public ResponseEntity<Long> cout()
-	{
+	@RequestMapping(value = "/regions/count", method = RequestMethod.GET)
+	public ResponseEntity<Long> count()
+	{		
 		return new ResponseEntity<Long>(regionsService.count(), HttpStatus.OK);
 	}
 
