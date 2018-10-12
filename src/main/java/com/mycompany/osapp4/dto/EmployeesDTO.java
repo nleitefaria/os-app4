57678fb93408dd18c3cd8f6d5fd5bcd0a41089bf
nleitefaria <nleitefaria@gmail.com>
2018-10-12 11:39:36 +0100

    clean

---
+++ src/main/java/com/mycompany/osapp4/dto/EmployeesDTO.java
@@ -0,0 +1,106 @@
+package com.mycompany.osapp4.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class EmployeesDTO 
+{
+	private int employeeId;
+	private String jobId;
+	private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Date hireDate;
+    private BigDecimal salary;
+    private BigDecimal commissionPct;
+	
+    public EmployeesDTO()
+    {
+    }
+
+	public EmployeesDTO(int employeeId, String jobId, String firstName, String lastName, String email, String phoneNumber, Date hireDate, BigDecimal salary, BigDecimal commissionPct) 
+	{
+		this.employeeId = employeeId;
+		this.jobId = jobId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.hireDate = hireDate;
+		this.salary = salary;
+		this.commissionPct = commissionPct;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public BigDecimal getSalary() {
+		return salary;
+	}
+
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+
+	public BigDecimal getCommissionPct() {
+		return commissionPct;
+	}
+
+	public void setCommissionPct(BigDecimal commissionPct) {
+		this.commissionPct = commissionPct;
+	}
+}
--- src/main/java/com/mycompany/osapp4/service/EmployeesService.java
+++ src/main/java/com/mycompany/osapp4/service/EmployeesService.java
@@ -2,6 +2,7 @@ package com.mycompany.osapp4.service;
 
 import java.util.List;
 
+import com.mycompany.osapp4.dto.EmployeesDTO;
 import com.mycompany.osapp4.entity.Employees;
 
 public interface EmployeesService 
@@ -9,5 +10,6 @@ public interface EmployeesService
 	Long count();
 	List<Employees> findAll();
 	Employees findOne(String id);
+	Employees save(EmployeesDTO employeesDTO);
 
 }
