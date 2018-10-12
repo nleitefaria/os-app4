2b62fe2d74251e10439cd4f912040dccdf4f15f5
nleitefaria <nleitefaria@gmail.com>
2018-10-12 12:22:31 +0100

    Added POST to Jobs

--- src/main/java/com/mycompany/osapp4/rws/JobsController.java
+++ src/main/java/com/mycompany/osapp4/rws/JobsController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mycompany.osapp4.dto.JobsDTO;
 import com.mycompany.osapp4.entity.Jobs;
 import com.mycompany.osapp4.service.JobsService;
 
@@ -40,5 +42,12 @@ public class JobsController
 	{		
 		return new ResponseEntity<Jobs>(service.findOne(id), HttpStatus.OK);
 	}
+	
+	@RequestMapping(value = "/job", method = RequestMethod.POST)
+	public ResponseEntity<Jobs> save(@RequestBody JobsDTO jobsDTO)
+	{
+		return new ResponseEntity<Jobs>(service.save(jobsDTO), HttpStatus.CREATED);
+	}
+	
 	
 }
