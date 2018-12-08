6a3b272d97a6961e2a7eebcc53ddd3dcb112aeb4
nleitefaria <nleitefaria@gmail.com>
2018-12-08 11:47:40 +0000

    Added CORS filter

--- src/main/java/com/mycompany/osapp4/Osapp4Application.java
+++ src/main/java/com/mycompany/osapp4/Osapp4Application.java
@@ -1,12 +1,30 @@
 package com.mycompany.osapp4;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Osapp4Application {
+public class Osapp4Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Osapp4Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Osapp4Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Osapp4Application.class);
+    }
+    
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
 }
